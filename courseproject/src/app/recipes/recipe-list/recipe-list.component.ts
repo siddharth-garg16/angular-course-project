218c17c8a1b08272ae917495cfcf16278e4e5468
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-02-25 17:33:09 +0530

    static prototype - for components

--- courseproject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ courseproject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe("A test Recipe", "This is simple a test", "https://www.washingtonpost.com/resizer/yDOe4WHPk6n6yZPrqMiTsZIhMEo=/arc-anglerfish-washpost-prod-washpost/public/ROBCBUCRT76UT24BGTWJ5UKUAY.jpg"),
+    new Recipe("A test Recipe", "This is simple a test", "https://www.washingtonpost.com/resizer/yDOe4WHPk6n6yZPrqMiTsZIhMEo=/arc-anglerfish-washpost-prod-washpost/public/ROBCBUCRT76UT24BGTWJ5UKUAY.jpg")
+  ];
 
+  ngOnInit(){
+
+  }
 }
